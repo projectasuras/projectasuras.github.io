741e164f130a6bd231db650274aa10cd3ab36b67
Project Asuras <projectasuras@proton.me>
2024-12-06 12:12:01 +0530

    Added pageTitleSuffix

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Project Asuras",
-    pageTitleSuffix: "",
+    pageTitleSuffix: " - Project Asuras",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
