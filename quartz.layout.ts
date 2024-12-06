6cdeb2b17cd738073740995c6c33d1cd0217c3f6
Project Asuras <projectasuras@proton.me>
2024-12-06 15:26:59 +0530

    Enabled explorer in mobile

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,9 +30,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.MobileOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.DesktopOnly(Component.Backlinks()),
   ],
 }
 
